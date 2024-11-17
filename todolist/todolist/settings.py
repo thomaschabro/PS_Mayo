357fed6b86df20f8769a20b98ad5a8197cbfda1c
thomaschabro <81168920+thomaschabro@users.noreply.github.com>
2024-11-17 15:48:58 +0100

    installed JWT package and added in settings

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
